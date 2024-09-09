5e8fe9763b03ea0ffc4a7daeb16ea8b7ed6478ec
Kalinin Dmirty <didiwot@yandex.ru>
2024-09-09 16:16:47 +0300

    Окно по центру

--- qt_learn/main.cpp
+++ qt_learn/main.cpp
@@ -5,7 +5,15 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
-    w.show();
 
+    w.setWindowTitle("Вывод окна по центру экрана");
+    w.resize(350, 70);
+
+    QScreen *screen = QApplication::primaryScreen();
+    QRect rect = screen->geometry();
+    int x = (rect.width() - w.width()) / 2;
+    int y = (rect.height() - w.height()) / 2;
+    w.move(x, y);
+    w.show();
     return a.exec();
 }
--- qt_learn/widget.h
+++ qt_learn/widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QScreen>
 
 namespace Ui {
 class Widget;
